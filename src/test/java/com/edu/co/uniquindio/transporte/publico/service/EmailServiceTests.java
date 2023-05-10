8f33d0ea7664c9b8e18e87013eb5a7ff798b1ad0
lolEstraski <carlos1andres20@gmail.com>
2023-05-09 21:23:39 -0700

    emailService test

---
+++ src/test/java/com/edu/co/uniquindio/transporte/publico/service/EmailServiceTests.java
@@ -0,0 +1,53 @@
+package com.edu.co.uniquindio.transporte.publico.service;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTests {
+
+    @Mock
+    private JavaMailSender javaMailSenderMock;
+
+    @InjectMocks
+    private EmailService emailService;
+
+
+    @Test
+    @DisplayName("Deberia enviar un email si los datos son correctos")
+    public void debeEnviarEmail(){
+        //given
+        var asuntoValido = "asunto";
+        var contenidoValido = "Contenido valido";
+        var destinatarioValido = "DestinatarioValido";
+        //when
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.example.com");
+        properties.put("mail.smtp.port", "25");
+        Mockito.when(javaMailSenderMock.createMimeMessage()).thenReturn(new MimeMessage(Session.getDefaultInstance(properties)));
+        var resultado = emailService.enviarEmail(asuntoValido, contenidoValido, destinatarioValido);
+        //then
+        Assertions.assertThat(resultado).isTrue();
+    }
+
+}
+
+
